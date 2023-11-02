43e2a2e96a8c3bc4290740361e8131ad442bb158
Milo Lee <143678373+ChronosWithA-K@users.noreply.github.com>
2023-11-02 20:06:59 +0000

    finish ~1/2 of double up seven

--- games/double_up_seven.py
+++ games/double_up_seven.py
@@ -6,6 +6,7 @@ import os
 playing = True
 player_has_bullet = False
 opponent_has_bullet = False
+player_just_reflected = False
 
 valid_responses = ['reload', 'block', 'reflect', 'shoot']
 
@@ -50,11 +51,38 @@ def lose():
     while play_again != 'yes' and play_again != 'no':
         play_again = input("That was not a valid response. Type in either yes or no. ")
 
+def tie():
+    """Logic for what happens when the player ties."""
+    global choice
+    global response
+    global player_has_bullet
+    global opponent_has_bullet
+
+    if player_has_bullet:
+        choice = input(f"""You tied (you chose {choice} and your opponent chose {response}).\
+ Choose either shoot, reflect, or block. """)
+
+        while choice not in valid_responses:
+            choice = input("""That was not a valid input. Choose either shoot, reflect, or block\
+ .""")
+            clear()
+        while choice == 'shoot':
+            choice = input ("""That was not a valid response. Choose either reload, block, or \
+ reflect. """)
+    else:
+        choice = input(f"""You tied (you chose {choice} and your opponent chose {response}).\
+ Choose either reload, reflect, or block. """)
+
+        while choice not in valid_responses:
+            choice = input("""That was not a valid input. Choose either reload, reflect, or block\
+ .""")
+            clear()
+
 clear()
 
 name = input("Hello user. What's your name? ")
 print(f"Well then, {name.title()}, are you ready to play some Double Up Seven? ")
-input("(enter to continue)")
+input("(enter to continue) ")
 clear()
 
 understand = input("""The rules of the game are as follows: when it's time to choose\
@@ -63,7 +91,8 @@ understand = input("""The rules of the game are as follows: when it's time to ch
  bullet). Blocking lets you block a bullet that your opponent shot. Reflecting lets you\
  send a bullet that your opponent shot back at them (you cannot do this twice in a row). Do you\
  understand (yes / no)? """)
-while understand != 'yes' and understand != 'no':
+clear()
+while understand != 'yes':
     understand = input("""The rules of the game are as follows: when it's time to choose\
  between reload, shoot, reflect, and block. Reloading gives you a bullet (you can only\
  have one bullet at a time). Shooting shoots your gun (you can only do this if you have a\
@@ -75,22 +104,55 @@ while understand != 'yes' and understand != 'no':
 while playing:
     # Logic for what the player can choose.
     if player_has_bullet:
-        choice = input("Do you want to block, reflect, or shoot?")
+        choice = input("Do you want to block, reflect, or shoot? ")
 
-        while choice not in valid_responses:
+        while choice not in valid_responses or choice == 'reload':
             choice = input("That was not a valid input. Do you want to block, reflect, or shoot? ")
             clear()
     else:
         choice = input("Do you want to reload, block, or reflect? ")
         clear()
 
-        while choice not in valid_responses:
+        while choice not in valid_responses or choice == 'shoot':
             choice = input("That was not a valid input. Do you want to reload, block, or reflect? ")
             clear()
 
+        if choice == 'reload':
+            player_has_bullet = True
+        elif choice == 'shoot':
+            player_has_bullet = False
+
     opponent_choice()
 
-    if choice != 'reflect' and response == 'shoot':
+    if choice == 'shoot' and response == 'reload':  # start shoot logic
         win()
-    else:
+    elif choice == 'shoot' and response == 'reflect':
         lose()
+    elif choice == 'shoot' and response == 'block':
+        tie()
+    elif choice == 'shoot' and response == 'block':
+        tie()
+    elif choice == 'reflect' and response == 'reload':  # start reflect logic
+        tie()
+    elif choice == 'reflect' and response == 'reflect':
+        tie()
+    elif choice == 'reflect' and response == 'shoot':
+        win()
+    elif choice == 'reflect' and response == 'block':
+        tie()
+    elif choice == 'reload' and response == 'reload':  # start reload logic
+        tie()
+    elif choice == 'reload' and response == 'shoot':
+        lose()
+    elif choice == 'reload' and response == 'block':
+        tie()
+    elif choice == 'reload' and response == 'reflect':
+        tie()
+    elif choice == 'block' and response == 'reload':  # start block logic
+        tie()
+    elif choice == 'block' and response == 'reflect':
+        tie()
+    elif choice == 'block' and response == 'shoot':
+        tie()
+    elif choice == 'block' and response == 'block':
+        tie()
